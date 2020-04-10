a3fd6ec171caae8f4c23624e1df7b99f4e191efc
Anirudh H M <anihm136@gmail.com>
2020-04-10 20:51:18 +0530

    Initial commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const getFilesFromDir = require("./config/files");
+const PAGE_DIR = path.join("src", "pages", path.sep);
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const htmlPlugins = getFilesFromDir(PAGE_DIR, [".html"]).map((filePath) => {
+  const fileName = filePath.replace(PAGE_DIR, "");
+  return new HtmlWebPackPlugin({
+    chunks: [fileName.replace(path.extname(fileName), ""), "vendor"],
+    template: filePath,
+    filename: fileName,
+  });
+});
+
+const entry = getFilesFromDir(PAGE_DIR, [".js"]).reduce((obj, filePath) => {
+  const entryChunkName = filePath
+    .replace(path.extname(filePath), "")
+    .replace(PAGE_DIR, "");
+  obj[entryChunkName] = `./${filePath}`;
+  return obj;
+}, {});
+
+module.exports = {
+  entry: entry,
+  plugins: [...htmlPlugins, new MiniCssExtractPlugin()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "src"),
+      components: path.resolve(__dirname, "src", "components"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: "initial",
+          name: "vendor",
+          enforce: true,
+        },
+      },
+    },
+  },
+  devServer: {
+    port: 3100,
+    compress: true,
+    stats: "errors-only",
+    open: true,
+    inline: true,
+    hot: true,
+  },
+};
